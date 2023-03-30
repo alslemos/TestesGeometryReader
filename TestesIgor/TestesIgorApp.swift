db106f91a2373218a1b94bb3d24d9d0ec5902da9
alslemos <alexandre-ls@hotmai.com>
2023-03-30 13:49:20 -0300

    Initial Commit

---
+++ TestesIgor/TestesIgorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestesIgorApp.swift
+//  TestesIgor
+//
+//  Created by Alexandre Lemos da Silva on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TestesIgorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
